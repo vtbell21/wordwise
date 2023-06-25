f31d292edbeecd0cbae93172c9d2f1eb5fed796f
Taylor Bell <56656419+vtbell21@users.noreply.github.com>
2023-06-25 13:20:24 -0600

    Configurement changes

--- myVenv/Scripts/activate.bat
+++ myVenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\greas\OneDrive\Desktop\Word Wise\myVenv
+set VIRTUAL_ENV=C:\Users\greas\OneDrive\Desktop\Word Wise\myProject\myVenv
 
 if not defined PROMPT set PROMPT=$P$G
 
